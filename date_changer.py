b90beb8c0d80396a509a4c543c3c9346204c8636
BugzzzC <bugsbunnyes@icloud.com>
2025-01-26 02:33:29 -1000

    Add files via upload

---
+++ date_changer.py
@@ -0,0 +1,15 @@
+created_date = "19 марта 2024"
+issue_date = "19.03.24"
+
+temp_created_date = created_date
+temp_issue_date = issue_date
+
+print(created_date)
+print(issue_date)
+
+print(temp_created_date[0:9:1])
+print(temp_issue_date[0:5:1])
+
+
+
+
